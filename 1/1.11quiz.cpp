13a3d217e944465004f3a1b8d660d4794414f28f
Yong Seok Lee <lyeongsuk97@gmail.com>
2023-12-27 22:11:52 +0900

    finished section 1

---
+++ 1/1.11quiz.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main() {
+    int x{};
+    std::cout << "enter an int: ";
+    std::cin >> x;
+    std::cout << "int * 2: " << x * 2 << '\n';
+    std::cout << "int * 3: " << x * 3 << '\n';
+}
