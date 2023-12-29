82d655ddc076b7a9fa972affc1ea9b519db10196
Yong Seok Lee <lyeongsuk97@gmail.com>
2023-12-29 16:57:17 +0900

    finished section 2 and 3

---
+++ 0.7/foo.h
@@ -0,0 +1,8 @@
+//
+// Created by Yong Seok Lee on 12/29/23.
+//
+
+#ifndef INC_0_7_FOO_H
+#define INC_0_7_FOO_H
+
+#endif //INC_0_7_FOO_H
---
+++ 0.7/io.h
@@ -0,0 +1,9 @@
+#ifndef ABC
+#define ABC
+
+int add(int x, int y)
+{
+    return x + y;
+}
+
+#endif
